bd4fd660d8c1240a472bf77b3dab554a875424c2
ycsal <yascsalgado@gmail.com>
2026-07-17 02:02:07 -0300

    evitando falhas

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 function sortear(){
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
-    let ate = parseInt(document.getElementById("ate").value)            ;
+    let ate = parseInt(document.getElementById("ate").value);
+
+    if (de > ate) {
+        alert("O valor 'do número' não pode ser maior que o valor 'até o número'.");
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert("A quantidade de números sorteados não pode ser maior que a quantidade de números disponíveis no intervalo.");
+        return;
+    }
+
+    
 
     let sorteados = [];
     let numero;
@@ -12,15 +24,35 @@ function sortear(){
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
         }
-        
+
         sorteados.push(numero);
     }
 
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados.join(", ")}</label>`;
 
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else{
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar() { 
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";  
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>'
+    alterarStatusBotao();
 }
